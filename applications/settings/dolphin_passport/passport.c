1ca4d7fccbf5c53a7baeacd1b60a14ed61e1ce62
GameLord2011 <119822417+GameLord2011@users.noreply.github.com>
2024-10-25 17:49:02 -0400

    Update passport.c

--- applications/settings/dolphin_passport/passport.c
+++ applications/settings/dolphin_passport/passport.c
@@ -40,13 +40,13 @@ static void render_callback(Canvas* canvas, void* _ctx) {
 
     if(stats->butthurt <= 4) {
         portrait = &I_passport_happy_46x49;
-        mood_str = "Mood: Happy";
+        mood_str = "Mood: 0_o<Happy";
     } else if(stats->butthurt <= 9) {
         portrait = &I_passport_okay_46x49;
-        mood_str = "Mood: Okay";
+        mood_str = "Mood: 0_o<Just Ok";
     } else {
         portrait = &I_passport_bad_46x49;
-        mood_str = "Mood: Angry";
+        mood_str = "Mood: 0_o<Pissed  Off";
     }
 
     uint32_t xp_progress = 0;
